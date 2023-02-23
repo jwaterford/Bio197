f36828413ee7da4bc855e24cfd25d6d5c2a68a62
jwaterford <jwaterford@ucsd.edu>
2023-02-23 13:36:38 -0800

    goodbye old files

---
+++ Python/PyTesting/Untitled-1.py
@@ -0,0 +1,12 @@
+
+
+
+##Create a pathway for Trees to Roads##
+
+os.chdir("/Users/slimjims/Desktop/01100011/Python/QSB")
+treefile_path = files/simtree.tre
+
+## We need to select a base genome, and how many "variable sites" we will consider ##
+
+base_genome_name = gi
+base_genonme_path = ref/mini_ref.fasta
