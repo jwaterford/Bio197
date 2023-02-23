794e97bf722be4925d5a8f2597ea0db303e78e22
jwaterford <jwaterford@ucsd.edu>
2023-02-23 12:38:27 -0800

    All base files moved to bio, instead of main

---
+++ 197-scripts/LunaRProjectW3.R
@@ -0,0 +1,28 @@
+getwd()
+
+#~~BASIC ARITHMETIC~~#
+
+2-10
+3*5
+9/2
+5-3*2
+(5-3)*2
+4^2
+8/2^2
+
+# Mass Conversion #
+
+mass_lbs <- 3.5
+mass_kg <- mass_lbs*0.454
+mass_kg
+
+# Built in Functions #
+
+abs(-15.5)
+round(4.48347, digits = 1)
+round(3.8, digits = 0)
+SPECIES <- "species"
+species <- "SPECIES"
+Var <- sqrt(2.6)
+(Var_rounded <- round(Var, digits = 2))
+
